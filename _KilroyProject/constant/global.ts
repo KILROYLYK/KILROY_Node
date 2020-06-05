2a66c27e62840ae1e133ed59c8a54b937eef1afb
kilroy_lyk <736587340@qq.com>
2020-06-05 16:16:59 +0800

    KILROY

--- _KilroyProject/constant/global.ts
+++ _KilroyProject/constant/global.ts
@@ -6,7 +6,7 @@ import DataBase from '../database/_DataBase'; // 数据库
  * 全局
  */
 export default class Global {
-    public static readonly port: '3000'; // 端口（0-65536)
+    public static readonly port: number | string = 3000; // 端口（0-65536)
     
     public static readonly Function: any = Function;
     public static readonly Status: any = Status;
